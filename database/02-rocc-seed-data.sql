0a83b832fecd0d0b01b47a3e67929e95678738d9
RePierre <petru.rebeja@gmail.com>
2021-09-20 20:45:47 +0300

    Updated syntax of seed-data script

--- database/02-rocc-seed-data.sql
+++ database/02-rocc-seed-data.sql
@@ -1,78 +1,78 @@
-INSERT INTO `accoladeDirections` (`id`) VALUES
+INSERT INTO accoladedirections (id) VALUES
 ('left'),
 ('right');
 
-INSERT INTO `alignmentTypes` (`id`) VALUES
+INSERT INTO alignmenttypes (id) VALUES
 ('gold'),
 ('test');
 
-INSERT INTO `annotationLevels` (`id`, `name`) VALUES
+INSERT INTO annotationlevels (id, name) VALUES
 ('g', 'gold'),
 ('m', 'mixt'),
 ('o', 'original'),
 ('t', 'test');
 
-INSERT INTO `bookFormats` (`id`) VALUES
+INSERT INTO bookformats (id) VALUES
 (' '),
 ('duo'),
 ('folio'),
 ('octavo'),
 ('quarto');
 
-INSERT INTO `centuries` (`id`) VALUES
+INSERT INTO centuries (id) VALUES
 ('XIX'),
 ('XVI'),
 ('XVII'),
 ('XVIII');
 
-INSERT INTO `columnPositions` (`id`, `name`) VALUES
+INSERT INTO columnpositions (id, name) VALUES
 ('L', 'Left'),
 ('R', 'Right'),
 ('U', 'Unique');
 
-INSERT INTO `corrections` (`id`) VALUES
+INSERT INTO corrections (id) VALUES
 ('few'),
 ('many'),
 ('none');
 
-INSERT INTO `difficultyLevels` (`id`, `name`) VALUES
+INSERT INTO difficultylevels (id, name) VALUES
 (1, 'easy'),
 (3, 'hard'),
 (2, 'medium');
 
-INSERT INTO `halfCenturies` (`id`) VALUES
+INSERT INTO halfcenturies (id) VALUES
 (1),
 (2);
 
-INSERT INTO `lineTypes` (`id`) VALUES
+INSERT INTO linetypes (id) VALUES
 ('footer'),
 ('header'),
 ('ordinary');
 
-INSERT INTO `marginalWritings` (`id`) VALUES
+INSERT INTO marginalwritings (id) VALUES
 ('few'),
 ('many'),
 ('none');
 
-INSERT INTO `provinces` (`id`, `name`) VALUES
+INSERT INTO provinces (id, name) VALUES
 (4, ' '),
 (1, 'Moldova'),
 (2, 'Țara Românească'),
 (3, 'Transilvania');
 
-INSERT INTO `sheetTypes` (`id`) VALUES
+INSERT INTO sheettypes (id) VALUES
 ('n/a'),
 ('page'),
 ('sheet');
 
-INSERT INTO `writingTypes` (`id`, `name`) VALUES
+INSERT INTO writingtypes (id, name) VALUES
 ('m', 'manuscript'),
 ('p', 'print'),
 ('su', 'semiuncial'),
 ('u', 'uncial'),
 ('', 'unknown');
 
-INSERT INTO `zones` (`id`, `name`) VALUES
+INSERT INTO zones (id, name) VALUES
 ('BT', 'Banat'),
 ('BS', 'Bessarabia'),
 ('MM', 'Maramureș'),
